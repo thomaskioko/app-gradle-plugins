4289c299619585ef75412e076a58ea53fdc7723d
Thomas Kioko <kiokotomas@gmail.com>
2025-08-24 16:33:19 +0200

    Add project configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
+rootProject.name = "app-gradle-plugins"
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex(".*google.*")
+                includeGroupByRegex(".*android.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex(".*google.*")
+                includeGroupByRegex(".*android.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+include(":plugins")
