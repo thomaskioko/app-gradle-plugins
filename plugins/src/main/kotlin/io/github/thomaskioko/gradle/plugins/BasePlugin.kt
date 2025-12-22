a68b832e964c8188be3b6d6c467d40c389b99743
Thomas Kioko <kiokotomas@gmail.com>
2025-12-22 17:15:09 +0100

    Enable return-value-checker

--- plugins/src/main/kotlin/io/github/thomaskioko/gradle/plugins/BasePlugin.kt
+++ plugins/src/main/kotlin/io/github/thomaskioko/gradle/plugins/BasePlugin.kt
@@ -69,6 +69,7 @@ public abstract class BasePlugin : Plugin<Project> {
                     "-Xcontext-parameters",
                     "-Xcontext-sensitive-resolution",
                     "-Xannotation-target-all",
+                    "-Xreturn-value-checker=full",
                     // opt in to experimental apis
                     "-opt-in=kotlin.time.ExperimentalTime",
                     "-opt-in=kotlin.uuid.ExperimentalUuidApi",
