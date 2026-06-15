7363b6d7b38063d3f577eb4eaa234c6295c6419f
Thomas Kioko <kiokotomas@gmail.com>
2026-06-15 17:08:46 +0200

    Add kotlin-metadata-jvm to settings buildscripts

--- codegen/settings.gradle.kts
+++ codegen/settings.gradle.kts
@@ -2,6 +2,15 @@ pluginManagement {
     includeBuild("../build-logic")
 }
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-metadata-jvm:2.4.0")
+    }
+}
+
 plugins {
     id("dependency-config")
     id("publishing-config")
